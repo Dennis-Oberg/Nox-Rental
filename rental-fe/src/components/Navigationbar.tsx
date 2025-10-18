238956d5af525034a3ae2aa2804e8d14aedd50de
Dennis Öberg <dennis.kg.oberg@gmail.com>
2025-10-19 00:48:45 +0200

    first commit

---
+++ rental-fe/src/components/Navigationbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navigationbar = () => {
+    return (
+        <div className={"navigationcontainer"}>
+            <ul>
+                <li>
+                    <a href="">Rent a car</a>
+                </li>
+                <li>
+                    <a href="">Admin</a>
+                </li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default Navigationbar;
